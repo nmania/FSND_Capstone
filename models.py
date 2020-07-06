64632220297078f01470562033a2a0b274ec68a1
Eric Hiltz <eric.m.hiltz@gmail.com>
2020-07-06 15:07:57 -0400

    Small changes to try and get the heroku updates to work

--- models.py
+++ models.py
@@ -27,19 +27,19 @@ def setup_db(app, database_path=database_path):
 Person
 Have title and release year
 '''
-class Person(db.Model):  
-  __tablename__ = 'People'
+# class Person(db.Model):  
+#   __tablename__ = 'People'
 
-  id = Column(Integer, primary_key=True)
-  name = Column(String)
-  catchphrase = Column(String)
+#   id = Column(Integer, primary_key=True)
+#   name = Column(String)
+#   catchphrase = Column(String)
 
-  def __init__(self, name, catchphrase=""):
-    self.name = name
-    self.catchphrase = catchphrase
+#   def __init__(self, name, catchphrase=""):
+#     self.name = name
+#     self.catchphrase = catchphrase
 
-  def format(self):
-    return {
-      'id': self.id,
-      'name': self.name,
-      'catchphrase': self.catchphrase}
+#   def format(self):
+#     return {
+#       'id': self.id,
+#       'name': self.name,
+#       'catchphrase': self.catchphrase}
