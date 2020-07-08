4c54efbef66c2d1e50b82457442df93041fce03c
Eric Hiltz <eric.m.hiltz@gmail.com>
2020-07-08 14:41:56 -0400

    Got all basic tests working

--- test_app.py
+++ test_app.py
@@ -7,7 +7,7 @@ from app import create_app
 from models import setup_db, Movie, Actor
 
 DATABASE_NAME = "capstone_test"
-DATABASE_PATH = "postgresql://{}/{}".format('localhost:5432', DATABASE_NAME)
+DATABASE_PATH = "postgresql://{}/{}".format('postgres@localhost:5432', DATABASE_NAME)
 
 # Actor Tests
 class ActorTestCase(unittest.TestCase):
@@ -56,18 +56,76 @@ class ActorTestCase(unittest.TestCase):
         self.assertTrue(data['actors'])
 
     # GET /actors bad
+    def test_get_actors_bad(self):
+        """Test that get actors returns 405 due to a bad call"""
+        res = self.client().get('/actors/3')
+        data = json.loads(res.data)
+        self.assertEqual(res.status_code, 405)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'Request not allowed')
 
     # DELETE /actors good
+    def test_delete_actor_good(self):
+        """Test to make sure an actor can be deleted"""
+        actor_id = 1
+        res = self.client().delete('/actors/'+str(actor_id))
+        data = json.loads(res.data)
+        actor = Actor.query.filter(Actor.id == actor_id).one_or_none()
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertEqual(data['deleted'], actor_id)
+        self.assertEqual(actor,None)
 
     # DELETE /actors bad
+    def test_delete_actor_bad(self):
+        """Test to make sure a non-existing actor deletion attempt returns a 404"""
+        actor_id = 999999
+        res = self.client().delete('/actors/'+str(actor_id))
+        data = json.loads(res.data)
+        actor = Actor.query.filter(Actor.id == actor_id).one_or_none()
+        self.assertEqual(res.status_code, 404)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'Resource Not Found')
 
     # POST /actors good
+    def test_post_actor_good(self):
+        """Test to make sure an actor can be added"""
+        res = self.client().post('/actors',json=self.new_actor)
+        data = json.loads(res.data)
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(data['created'])
 
     # POST /actors bad
+    def test_post_actor_bad(self):
+        """Test to make sure a poorly formed actor json will retun a 422"""
+        res = self.client().post('/actors',json=self.bad_actor)
+        data = json.loads(res.data)
+        self.assertEqual(res.status_code, 422)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'Unprocessable')
 
     # PATCH /actors good
+    def test_update_actor_good(self):
+        """Test to make sure an actor can be updated"""
+        actor_id = 2
+        res = self.client().patch('/actors/'+str(actor_id), json={'age': 100})
+        data = json.loads(res.data)
+        actor = Actor.query.filter(Actor.id == actor_id).one_or_none()
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertEqual(actor.format()['id'], actor_id)
 
     # PATCH /actors bad
+    def test_update_actor_bad(self):
+        """Test to make sure a non-existing actor post attempt returns a 404"""
+        actor_id = 99999
+        res = self.client().patch('/actors/'+str(actor_id), json={'age': 100})
+        data = json.loads(res.data)
+        self.assertEqual(res.status_code, 404)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'Resource Not Found')
 
 
 # Movie Tests
@@ -82,7 +140,7 @@ class MovieTestCase(unittest.TestCase):
         self.database_path = DATABASE_PATH
         setup_db(self.app, self.database_path)
 
-        # actor for successful add
+        # movie for successful add
         self.new_movie ={
             'title': 'Working test movie', 
             'releaseDate': 'Thu, 20 Jan 2011 00:00:00 GMT'
@@ -110,24 +168,80 @@ class MovieTestCase(unittest.TestCase):
         """Test that gets movies returns 200"""
         res = self.client().get('/movies')
         data = json.loads(res.data)
-        print('Movies:',data)
         self.assertEqual(res.status_code, 200)
         self.assertEqual(data['success'], True)
         self.assertTrue(data['movies'])
 
     # GET /movies bad
+    def test_get_movies_bad(self):
+        """Test that get movies returns 405 due to a bad call"""
+        res = self.client().get('/movies/3')
+        data = json.loads(res.data)
+        self.assertEqual(res.status_code, 405)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'Request not allowed')
 
     # DELETE /movies good
+    def test_delete_movie_good(self):
+        """Test to make sure a movie can be deleted"""
+        movie_id = 1
+        res = self.client().delete('/movies/'+str(movie_id))
+        data = json.loads(res.data)
+        movie = Movie.query.filter(Movie.id == movie_id).one_or_none()
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertEqual(data['deleted'], movie_id)
+        self.assertEqual(movie,None)
 
     # DELETE /movies bad
+    def test_delete_movie_bad(self):
+        """Test to make sure a non-existing movie deletion attempt returns a 404"""
+        movie_id = 999999
+        res = self.client().delete('/movies/'+str(movie_id))
+        data = json.loads(res.data)
+        movie = Movie.query.filter(Movie.id == movie_id).one_or_none()
+        self.assertEqual(res.status_code, 404)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'Resource Not Found')
 
     # POST /movies good
+    def test_post_movie_good(self):
+        """Test to make sure a movie can be added"""
+        res = self.client().post('/movies',json=self.new_movie)
+        data = json.loads(res.data)
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(data['created'])
 
     # POST /movies bad
+    def test_post_movie_bad(self):
+        """Test to make sure a poorly formed movie json will retun a 422"""
+        res = self.client().post('/movies',json=self.bad_movie)
+        data = json.loads(res.data)
+        self.assertEqual(res.status_code, 422)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'Unprocessable')
 
     # PATCH /movies good
+    def test_update_movie_good(self):
+        """Test to make sure a movie can be updated"""
+        movie_id = 2
+        res = self.client().patch('/movies/'+str(movie_id), json={'title': 'New Title'})
+        data = json.loads(res.data)
+        movie = Movie.query.filter(Movie.id == movie_id).one_or_none()
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertEqual(movie.format()['id'], movie_id)
 
     # PATCH /movies bad
+    def test_update_movie_bad(self):
+        """Test to make sure a non-existing movie post attempt returns a 404"""
+        movie_id = 99999
+        res = self.client().patch('/movies/'+str(movie_id), json={'title': 'New Title'})
+        data = json.loads(res.data)
+        self.assertEqual(res.status_code, 404)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'Resource Not Found')
 
 
 # Make the tests conveniently executable
