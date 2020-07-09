2a3f1167bc227145b8d3946c990e01fe2d103dc7
Eric Hiltz <eric.m.hiltz@gmail.com>
2020-07-09 12:08:53 -0400

    Added 401 error handlers Moved authorization functions to auth.py

--- app.py
+++ app.py
@@ -4,129 +4,7 @@ from flask import Flask, jsonify, request, abort
 from models import setup_db, Actor, Movie
 from flask_cors import CORS
 import json
-
-# for auth zero
-AUTH0_DOMAIN = 'roofuseat.us.auth0.com'
-ALGORITHMS = ['RS256']
-API_AUDIENCE = 'image'
-
-# For rasing errors
-class AuthError(Exception):
-    def __init__(self, error, status_code):
-        self.error = error
-        self.status_code = status_code
-
-# Use to get the access token from a header
-def get_token_auth_header():
-    """Obtains the Access Token from the Authorization Header
-    """
-    auth = request.headers.get('Authorization', None)
-    if not auth:
-        raise AuthError({
-            'code': 'authorization_header_missing',
-            'description': 'Authorization header is expected.'
-        }, 401)
-
-    parts = auth.split()
-    if parts[0].lower() != 'bearer':
-        raise AuthError({
-            'code': 'invalid_header',
-            'description': 'Authorization header must start with "Bearer".'
-        }, 401)
-
-    elif len(parts) == 1:
-        raise AuthError({
-            'code': 'invalid_header',
-            'description': 'Token not found.'
-        }, 401)
-
-    elif len(parts) > 2:
-        raise AuthError({
-            'code': 'invalid_header',
-            'description': 'Authorization header must be bearer token.'
-        }, 401)
-
-    token = parts[1]
-    return token
-
-# use to verify the jwt
-def verify_decode_jwt(token):
-    jsonurl = urlopen(f'https://{AUTH0_DOMAIN}/.well-known/jwks.json')
-    jwks = json.loads(jsonurl.read())
-    unverified_header = jwt.get_unverified_header(token)
-    rsa_key = {}
-    if 'kid' not in unverified_header:
-        raise AuthError({
-            'code': 'invalid_header',
-            'description': 'Authorization malformed.'
-        }, 401)
-
-    for key in jwks['keys']:
-        if key['kid'] == unverified_header['kid']:
-            rsa_key = {
-                'kty': key['kty'],
-                'kid': key['kid'],
-                'use': key['use'],
-                'n': key['n'],
-                'e': key['e']
-            }
-    if rsa_key:
-        try:
-            payload = jwt.decode(
-                token,
-                rsa_key,
-                algorithms=ALGORITHMS,
-                audience=API_AUDIENCE,
-                issuer='https://' + AUTH0_DOMAIN + '/'
-            )
-
-            return payload
-
-        except jwt.ExpiredSignatureError:
-            raise AuthError({
-                'code': 'token_expired',
-                'description': 'Token expired.'
-            }, 401)
-
-        except jwt.JWTClaimsError:
-            raise AuthError({
-                'code': 'invalid_claims',
-                'description': 'Incorrect claims. Please, check the audience and issuer.'
-            }, 401)
-        except Exception:
-            raise AuthError({
-                'code': 'invalid_header',
-                'description': 'Unable to parse authentication token.'
-            }, 400)
-    raise AuthError({
-                'code': 'invalid_header',
-                'description': 'Unable to find the appropriate key.'
-            }, 400)
-
-# used to check if the permissions needed are in the payload
-def check_permisions(permissions,payload):
-    if 'permissions' not in payload: # Check if permissions are present
-        abort(401)
-    if permissions not in payload['permissions']: # check if the correct permissions are present
-        abort(403)
-    return True
-
-# Define the requires authorization decorator
-def requires_auth(permissions=''):
-    def requires_auth_decorator(f):
-        @wraps(f)
-        def wrapper(*args, **kwargs):
-            token = get_token_auth_header() # get the jwt
-            try:
-                payload = verify_decode_jwt(token) # veryfy the token
-            except:
-                abort(401)
-            check_permisions(permissions,payload) #check permissions too
-            return f(payload, *args, **kwargs)
-        return wrapper
-    return requires_auth_decorator
-
-
+from auth import AuthError, requires_auth
 
 def create_app(test_config=None):
 
@@ -363,6 +241,15 @@ def create_app(test_config=None):
             "message": "Resource Not Found",
             "sys_error": str(error)
         }), 404
+
+    @app.errorhandler(401)
+    def not_found(error):
+        return jsonify({
+            "success": False,
+            "error": 401,
+            "message": "Unauthorized",
+            "sys_error": str(error)
+        }), 401
 
     @app.errorhandler(422)
     def unprocessable(error):
