cda5eb82d407ae768b45f3caf3f8f1b5f75e01e0
Eric Hiltz <eric.m.hiltz@gmail.com>
2020-07-11 12:04:52 -0400

    Made auth0 global variables env variables

--- auth.py
+++ auth.py
@@ -3,11 +3,13 @@ from flask import request, _request_ctx_stack, abort
 from functools import wraps
 from jose import jwt
 from urllib.request import urlopen
+import os
 
 # for auth zero
-AUTH0_DOMAIN = 'roofuseat.us.auth0.com'
-ALGORITHMS = ['RS256']
-API_AUDIENCE = 'capstone'
+AUTH0_DOMAIN = os.environ.get('AUTH0_DOMAIN')
+ALGORITHMS = os.environ.get('ALGORITHMS')
+API_AUDIENCE = os.environ.get('API_AUDIENCE')
+
 
 # AuthError Exception
 '''
